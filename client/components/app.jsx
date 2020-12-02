a65acc8ba7f5ceef2a62f3d89d428faaac1b2b31
Cody Miller <cody.miller@learningfuze.com>
2020-12-02 23:46:00 +0000

    Completed implementation of ProductDetails render method

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -26,8 +26,8 @@ export default class App extends React.Component {
         <Header />
         <div className="container">
           <ProductList />
+          <ProductDetails productId={1} />
         </div>
-        <ProductDetails productId={1} />
       </>
     );
   }
--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -15,6 +15,28 @@ export default class ProductDetails extends React.Component {
   }
 
   render() {
-    return null;
+    const { product } = this.state;
+    if (!product) return <h1>Loading...</h1>;
+    return (
+      <div className="row">
+        <div className="card col-12 mt-3">
+          <div className="card-body d-flex flex-wrap">
+            <div className="col-12 p-3">
+              <button className="btn btn-secondary">
+                <i className="fas fa-arrow-left mr-2"></i>
+                Back To List
+              </button>
+            </div>
+            <img className="product-image col-5" src={product.image} alt={product.name} />
+            <div className="col-7 mt-3">
+              <h4>{product.name}</h4>
+              <p className="text-secondary">{`$${(Number(product.price) / 100).toFixed(2)}`}</p>
+              <p>{product.shortDescription}</p>
+            </div>
+            <div className="col-12 mt-4">{product.longDescription}</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
