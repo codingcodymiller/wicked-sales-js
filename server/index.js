ab00acc7d342adfcfd1a03ae9edb4f20a2c8aa02
Cody Miller <cody.miller@learningfuze.com>
2020-12-03 01:02:16 +0000

    added GET endpoint for '/api/cart'

--- server/index.js
+++ server/index.js
@@ -48,6 +48,10 @@ app.get('/api/products/:productId', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/cart', (req, res, next) => {
+  res.status(200).json(JSON.stringify([]));
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
